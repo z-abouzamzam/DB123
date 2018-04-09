f7793f8e023da0c33d1a9d59898cdc2eb0a3491c
Roberto <ramercad@caltech.edu>
2018-04-09 16:22:33 -0700

    Add basic document class structure

---
+++ classes/document.py
@@ -0,0 +1,19 @@
+import json
+import io
+
+
+class Document:
+	"""
+	"""
+	def __init__(self, name, attributes={}):
+		try:
+		    to_unicode = unicode
+		except NameError:
+		    to_unicode = str
+
+		attributes['documentName'] = name
+		filename = '../storage/' + name + '.json'
+
+		with io.open(filename, 'w', encoding='utf8') as outfile:
+			str_ = json.dumps(attributes, indent=4, sort_keys=True, separators=(',', ': '), ensure_ascii=False)
+			outfile.write(to_unicode(str_))
