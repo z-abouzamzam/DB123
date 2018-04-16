281dbf1e3c00207b34cbe1fc28b62b1da99bd9f3
Roberto <ramercad@caltech.edu>
2018-04-15 20:08:35 -0700

    Modify read and delete functionality
    Change path prepends so creating a storage
    folder if one does not exist will not create
    it one level outside the database directory
    
    Move read function outside of Document class so other
    classes and functions can call it on document names.
    
    Add delete function which accepts document names.

--- classes/document.py
+++ classes/document.py
@@ -10,7 +10,7 @@ class Document:
         attributes['documentName'] = name
 
         self.name = name
-        self.filename = '../storage/' + name + '.json'
+        self.filename = 'storage/' + name + '.json'
         self.attributes = attributes
         self.write()
 
@@ -21,21 +21,25 @@ class Document:
         except NameError:
             to_unicode = str
 
-        if not os.path.exists('../storage/'):
-            os.makedirs('../storage/')
+        if not os.path.exists('storage/'):
+            os.makedirs('storage/')
 
         with io.open(self.filename, 'w', encoding='utf8') as outfile:
             str_ = json.dumps(self.attributes, indent=4, sort_keys=True,
                 separators=(',', ': '), ensure_ascii=False)
             outfile.write(to_unicode(str_))
 
-    def read(self):
-        with open(self.filename) as data_file:
-            loaded = json.load(data_file)
-        print (loaded)
-
     def update(self, changes):
         for key, value in changes.items():
             self.attributes[key] = value
         self.write()
 
+
+
+def read(filename):
+    with open('storage/' + filename + '.json') as data_file:
+        loaded = json.load(data_file)
+    return loaded
+
+def delete(filename):
+    os.remove('storage/' + filename + '.json')
