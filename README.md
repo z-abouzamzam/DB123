5d3c3240817545d77b9eb6fbd529d0c8fe8d5cf0
Zafir Abou-Zamzam <z.abouzamzam@gmail.com>
2018-04-15 22:41:09 -0700

    Added code to read existing documents

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DB123
+
+### Running the database:
+Currently, we have a semi working file to test and run the database. To run,
+simply go into DB123 directory and run `python build/runtest.py`
+At this point in the program, running the code from a different directory
+will cause issues. 
--- build/runtest.py
+++ build/runtest.py
@@ -2,14 +2,36 @@
 # for an actual interpreter for the DB
 
 import sys
+import os
 sys.path.append('classes')
 
 from document import *
 
+# some helper functions
+def readDoc(filename):
+    doc = read(filename)
+    # do some error check in document class
+    return doc
+
+def loadDocs():
+    docs = {}
+    if not os.path.exists('storage/'):
+        print('error: no files exist')
+        return
+
+    for file in os.listdir('storage/'):
+        filename = file[:-5]  # need to remove .json
+        print(filename)
+        docs[filename] = readDoc(filename)
+    return docs
+
+
 # for more commands, for now we can just add another elif and add
 # code to test the command
 if __name__ == '__main__':
-    documents = {}
+    # loaddocs not quite working..
+    documents = loadDocs()
+    print(documents['new'])
     while True:
         cmd = input('Do something: (c or u) ')
         if cmd == 'c':
@@ -19,15 +41,21 @@ if __name__ == '__main__':
             print('Input keys and values (q to quit)')
             while True:
                 key = input('key: ')
+                if key == 'q':
+                    break
                 value = input('value: ')
-                if key == 'q' or value == 'q':
+                if value == 'q':
                     break
                 data[key] = value
 
             d = Document(name, data)
             documents[name] = d
+
         elif cmd == 'u':
             name = input('name is ?: ')
+            if name not in documents.keys():
+                print('Document not found!')
+                continue
             data = {}
 
             # right now this supports multiple entries poorly,
@@ -35,15 +63,17 @@ if __name__ == '__main__':
             print('Input keys and values (q to quit)')
             while True:
                 key = input('key: ')
+                if key == 'q':
+                    break
                 value = input('value: ')
-                if key == 'q' or value == 'q':
+                if value == 'q':
                     break
                 data[key] = value
 
             # right now we are locally retrieving documents, but in the
             # future we are going to want to get the document from storage
             documents[name].update(data)
-
+            print(documents[name])
         # if the user doesn't specify an actual command, we will just quit
         else:
             exit()
