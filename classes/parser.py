64e7083cfabe9bbe69fcc944a6c60b5087e4b040
Zafir Abou-Zamzam <z.abouzamzam@gmail.com>
2018-04-30 15:27:27 -0700

    Fixed bugs, and added create template to code

--- classes/parser.py
+++ classes/parser.py
@@ -16,6 +16,8 @@ class Parser:
 
         if queryType == 'select':
             return self.find(query[1:])
+        elif queryType == 'create':
+            return self.create(query[1:])
         elif queryType == 'update':
             return self.update(query[1:])
         elif queryType == 'delete':
@@ -44,8 +46,8 @@ class Parser:
                     break
 
         if fpath != self.path:
-            doc = json.load(open(fpath))
-            if doc['collection'] == '0':
+            doc = json.load(open(fpath + '.json'))
+            if 'collection' not in doc.keys() or doc['collection'] == '0':
                 attr = {}
                 for i in attributes:
                     if i == '*':
@@ -65,7 +67,9 @@ class Parser:
 
         result = []
 
-        print("[")
+        # print("[")
+
+        # need to do file not found errors
 
         if fpath == self.path:
             for file in os.listdir(fpath):
@@ -83,25 +87,39 @@ class Parser:
         # else:
         #     pass
 
-        print("]")
+        # print("]")
 
         return
 
+    def create(self, query):
+        # document (int a 5, float b 4, c hello, int d, e)  - default to empty
+        # string, always need a value
+
+        # no empty fields
+
+        # all special chars we parse as string
+        # working on parsing
+
+        # query in form ['document', '(a', '5,', 'b', '4,', 'c', 'hello)']
+        docName = query[0]
+
+        i = 1
+        while(query[i + 1][-1] != ')'):
+            key = query[i]
+            try:
+                float(query[i + 1][:-1])
+            except ValueError:
+                pass
+            value = query[i + 1][:-1]
+
+
+
     def update(self, query):
+        # will look same as create, for now we will just delete old and create new
+        # update document (a 5, b 6, c hello)
         return
 
     def delete(self, query):
+        # delete from where syntax
+        # or just delete document (DELETE a)
         return
-
-for file in os.listdir('storage/'):
-    test = json.load(open('storage/'+file))
-    attr = ['documentName']
-    for i in attr:
-        print i in test.keys()
-
-
-
-
-
-
-
