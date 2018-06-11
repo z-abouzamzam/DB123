7601683b0078b683ab96a20cb0b487065bd36b95
Roberto <ramercad@caltech.edu>
2018-06-10 20:09:07 -0700

    Improve documentation for document and testing files

--- classes/document.py
+++ classes/document.py
@@ -3,11 +3,11 @@ import io
 import os
 
 class Document:
-    """
+    '''
     The Document class is the basis for all data storage in the database.
     Creating a document accepts a name and optionally a dictionary of
     attributes and values and stores this data in a JSON object.
-    """
+    '''
     def __init__(self, name, attributes={}):
 
         attributes['documentName'] = name
@@ -24,15 +24,20 @@ class Document:
         except NameError:
             to_unicode = str
 
+        # Create a storage folder if one does not already exist
         if not os.path.exists('storage/'):
             os.makedirs('storage/')
 
+        # Create a document using io and json functionality with the
+        # desired attributes from a dictionary
         with io.open(self.filename, 'w', encoding='utf8') as outfile:
             str_ = json.dumps(self.attributes, indent=4, sort_keys=True,
                 separators=(',', ': '), ensure_ascii=False)
             outfile.write(to_unicode(str_))
 
     def update(self, changes):
+        # Given a dictionary of changes modify the values in the object's
+        # attributes and then overwrite the json document
         for key, value in changes.items():
             self.attributes[key] = value
         self.write()
@@ -40,9 +45,11 @@ class Document:
 
 
 def read(filename):
+    # Display the contents of a document with the passed in filename
     with open('storage/' + filename + '.json') as data_file:
         loaded = json.load(data_file)
     return loaded
 
 def delete(filename):
+    # Delete a document with a given file name
     os.remove('storage/' + filename + '.json')
--- classes/testing.py
+++ classes/testing.py
@@ -3,8 +3,11 @@ import readline
 from parser import *
 
 if __name__ == "__main__":
+    # Initialize a parser for handling user input
     p = Parser()
     print("Welcome to DB123\n")
+
+    # Run the database until 'q' or 'quit' is issued as a command
     while True:
         cmd = input('>>> ')
         if(cmd.lower() == 'q' or cmd.lower() == 'quit'):
